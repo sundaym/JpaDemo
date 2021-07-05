8f75d0448791778136a8c836755dc54e6e7cc04a
Mew <sunday2011s@gmail.com>
2021-07-05 15:15:37 +0800

    add Junit

--- src/test/java/com/demo/service/EmployeeServiceTest.java
+++ src/test/java/com/demo/service/EmployeeServiceTest.java
@@ -1,7 +1,7 @@
-package com.demo.test;
+package com.demo.service;
 
 import com.demo.model.Employee;
-import com.demo.service.EmployeeService;
+import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -9,41 +9,64 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 public class EmployeeServiceTest {
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
+    private EntityManager em = emf.createEntityManager();
+    private EmployeeService service = new EmployeeService(em);
 
-    public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService");
-        EntityManager em = emf.createEntityManager();
-        EmployeeService service = new EmployeeService(em);
-
+    @Test
+    public void testCreateEmployee() {
         // create and persist employee
         em.getTransaction().begin();
         service.createEmployee(1, "Tom", 30000L);
         service.createEmployee(2, "John", 40000L);
-        service.createEmployee(3, "", 50000L);
+        service.createEmployee(3, "Ana", 50000L);
         em.getTransaction().commit();
+        // close the EM and EMF when done
+        em.close();
+        emf.close();
+    }
 
+    @Test
+    public void testFindEmployee() {
         // find a specific employee
         Employee emp = service.findEmployee(1);
         System.out.println(emp);
+        // close the EM and EMF when done
+        em.close();
+        emf.close();
+    }
 
+    @Test
+    public void testFindAllEmployees() {
         // find all employees
         List<Employee> allEmployees = service.findAllEmployees();
         for (Employee employee : allEmployees) {
             System.out.println(employee);
         }
+        // close the EM and EMF when done
+        em.close();
+        emf.close();
+    }
 
+    @Test
+    public void testUpdateEmployee() {
         // update the employee
         em.getTransaction().begin();
         Employee employee = service.raiseEmployeeSalary(1, 30000L);
         em.getTransaction().commit();
         System.out.println("Tom:" + employee);
+        // close the EM and EMF when done
+        em.close();
+        emf.close();
+    }
 
+    @Test
+    public void testRemoveEmployee() {
         // remove an employee
         em.getTransaction().begin();
         service.removeEmployee(3);
         em.getTransaction().commit();
         System.out.println("Removed Employee 3");
-
         // close the EM and EMF when done
         em.close();
         emf.close();
